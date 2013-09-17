879f331b49896b723db843d4d4e24e31f633032d
Paul Sawaya <me@paulsawaya.com>
2013-09-17 16:38:17 -0700

    basic templates, lib files for jquery and bootstrap

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import logging
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Apollo!'
+    return render_template('main_page.html')
 
 if __name__ == '__main__':
     app.config.update(DEBUG=True,PROPAGATE_EXCEPTIONS=True,TESTING=True)
