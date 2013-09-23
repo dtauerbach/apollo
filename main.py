284a78c6bdc293e2e1882f1794b32578a66152fa
Dan Auerbach <dan@eff.org>
2013-09-22 19:16:52 -0700

    Get flask-security working. Built-in forms need to be tied to modals from front page. Note incompatibility of flask-security and flask-csrf

--- main.py
+++ main.py
@@ -1,28 +1,67 @@
 import logging
+import os, sys
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.security import Security, SQLAlchemyUserDatastore, \
+    UserMixin, RoleMixin, login_required
+#from flaskext.csrf import csrf
 
-import os, sys
-
+# Create app
 app = Flask(__name__)
-
 try:
     app.secret_key = os.environ['SESSION_SECRET_KEY']
 except KeyError:
     print "Env vars missing. Did you run setupEnv.sh?"
     sys.exit(1)
+app.config['DEBUG'] = True
+app.config['SECRET_KEY'] = 'super-secret'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:root@localhost/testdb'
+app.config['SECURITY_CONFIRMABLE'] = True
+app.config['SECURITY_REGISTERABLE'] = True
+app.config['SECURITY_RECOVERABLE'] = True
+
+# Create database connection object
+db = SQLAlchemy(app)
+
+# Define models
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+
+class Role(db.Model, RoleMixin):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String(80), unique=True)
+    description = db.Column(db.String(255))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(255), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    confirmed_at = db.Column(db.DateTime())
+    roles = db.relationship('Role', secondary=roles_users,
+                            backref=db.backref('users', lazy='dynamic'))
+
+# Setup Flask-Security
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
+
+# Create a user to test with
+# @app.before_first_request
+# def create_user():
+#     db.create_all()
+#     user_datastore.create_user(email='matt@nobien.net', password='password')
+#     db.session.commit()
 
 # CSRF protection flaskext
-from flaskext.csrf import csrf
-csrf(app)
+#csrf(app)
 
 @app.route('/')
 def main_page():
     return render_template('main_page.html')
 
-@app.route('/sign_up', methods=['POST'])
-def sign_up():
-    return '/sign_up'
 
 if __name__ == '__main__':
     app.config.update(DEBUG=True,PROPAGATE_EXCEPTIONS=True,TESTING=True)
