8a23aab0cf047d718f273a084fc01ed529f11297
Paul Sawaya <me@paulsawaya.com>
2013-10-10 19:12:28 -0700

    searching the dashboard works

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,5 +1,41 @@
 $(document).ready(function() {
+    // Search field
+    $('#search_field').on('keyup', function() {
+        var typedTokens = $(this).val().split(' ');
+        var panel = $(this).closest('.panel');
 
+        // Field is empty, so show everything
+        if (typedTokens.length == 0) {
+            panel.find('.data-source').show();
+            return;
+        }
+
+        console.log($(panel).find('.data-source'))
+        $(panel).find('.data-source').each(function(idx, el) {
+            // Search the keywords and the data source name
+            var haystackTokens = $(el).attr('data-source-keywords').split(' ');
+            haystackTokens.push($(el).attr('data-source-name'));
+
+            // For each space-separated token the user types...
+            var tokenMatches = typedTokens.map(function(typedTok) {
+                // Return true if there's a substring match
+                return haystackTokens.map(function(tok) {
+                    return tok.indexOf(typedTok) != -1;
+                }).indexOf(true) != -1;
+            });
+            // If there's a single substring match between a typed token
+            // and one of the haystack tokens, show.
+            if (tokenMatches.indexOf(true) != -1) {
+                $(el).show();
+            }
+            else {
+                $(el).hide();   
+            }
+        });
+
+    });
+
+    // Make side menu collapsable
     $('#collapse_btn').on('click', function() {
         // Unhide uncollapse button
         $('#collapse_btn_col').removeClass('collapsed');
