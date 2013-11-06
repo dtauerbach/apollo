3ef3e576e6fc80d58b632c3890b85d0ab06282d5
Paul Sawaya <me@paulsawaya.com>
2013-11-06 12:37:37 -0800

    CSRF with ajax. unbreaks Persona login.

--- static/js/global.js
+++ static/js/global.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    var csrftoken = $('meta[name=csrf-token]').attr('content')
+
+    $.ajaxSetup({
+        beforeSend: function(xhr, settings) {
+            if (!/^(GET|HEAD|OPTIONS|TRACE)$/i.test(settings.type)) {
+                xhr.setRequestHeader("X-CSRFToken", csrftoken)
+            }
+        }
+    });
+
     $('.persona-login-btn').on('click', function() {
         function verifyAssertion(assertion) {
             $.ajax({
