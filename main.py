200c016bce0baa10c4ebd3c1176de585e31e5c4f
Iwein Fuld <iwein@fuld.net>
2013-11-12 15:27:51 +0100

    Reinstate authentication behavior in main.py

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ from flask.ext.security import Security, SQLAlchemyUserDatastore, \
 # Augment Flask's render_template with variables we want available everywhere
 def render_template(template_name, **kwargs):
     default_args = {
-       "user":current_user,
-       "login_user_form": security.login_form(),
-       "register_user_form": security.register_form()
+        "user":current_user,
+        "login_user_form": security.login_form(),
+        "register_user_form": security.register_form()
     }
     template_args = dict(kwargs.items() + default_args.items())
     return _render_template(template_name,**template_args)
@@ -73,8 +73,26 @@ def create_user():
     user_datastore.create_user(email='me@paulsawaya.com', password='batman',confirmed_at=datetime.now())
     db.session.commit()
 
+# @app.route('/')
+# def main_page():
+#     if current_user.is_authenticated():
+#         return redirect(url_for('dashboard'))
+#     return render_template('main_page.html')
+#
+
+@app.route('/services.json')
+def servicesjson():
+    if not current_user.is_authenticated():
+        return jsonify(dict(
+            error="You aren't authorized to view this."
+        ))
+    return render_template('services.json')
+
+
 @app.route('/')
 def home():
+    if current_user.is_authenticated():
+        return redirect(url_for('accountDashboard'))
     return render_template('pages/home.html')
 
 @app.route('/join')
@@ -98,12 +116,19 @@ def providers():
     return render_template('pages/providers.html', title = 'Health Care Providers', crumb = 'Health Care Providers')
 
 @app.route('/account/dashboard')
+@login_required
 def accountDashboard():
     return render_template('pages/account/dashboard.html', title = 'Your Data', crumb = 'View Data')
 
 @app.route('/account/settings')
+@login_required
 def accountSettings():
     return render_template('pages/account/settings.html', title = 'Manage Data Settings', crumb = 'Manage Data Settings')
+
+@app.route('/account/settings', methods=['POST'])
+@login_required
+def manage_data_page_post():
+    return "Your preference was %s" % (request.form['sharing-preference'])
 
 if __name__ == '__main__':
     app.config.update(DEBUG=True,PROPAGATE_EXCEPTIONS=True,TESTING=True)
