5e79a3253c7c701952ef361f2d2f13445ecc63a7
Jules Boussekeyt <jules.boussekeyt@gmail.com>
2014-01-04 18:20:41 +0100

    fix bad redirection to / when authenticated and starting app on protected page

--- client/source/js/services/check-authentication.js
+++ client/source/js/services/check-authentication.js
@@ -2,30 +2,49 @@ define(['./module'], function(services) {
 
   services.factory('CheckAuthentication', function($rootScope, $location, $http, User, SERVER_URL) {
 
+    var currentRoute, prevRoute;
+
     return {
+
+      applicationIsBootstraped: false,
+
       start: function () {
-        $rootScope.$on('$routeChangeStart', function(event, current, prev) {
-          // if current route requires Login, redirection
-          if (current.$$route.requireLogin && !User.authenticated) {
-            $location.path('/');
-          }
+        $rootScope.$on('$routeChangeStart', _.bind(function(event, current, prev) {
+          currentRoute = current;
+          prevRoute = prev;
 
-          // show/hide notifications
-          if (prev && prev.$$route.requireLogin) {
-            $rootScope.notification = {
-              type: 'warning',
-              message: 'You must be authenticated to access this page, please login.'
-            };
-          } else {
-            delete $rootScope.notification;
+          if (this.applicationIsBootstraped) {
+            this.checkRoute(current, prev);
           }
-        });
+        }, this));
 
-        $http.get(SERVER_URL + '/auth/check_authentication').success(function (resp) {
+        $http.get(SERVER_URL + '/auth/check_authentication').success(_.bind(function (resp) {
+          this.applicationIsBootstraped = true;
+
           if (resp.username) {
             angular.extend(User, resp, { authenticated: true });
           }
-        });
+
+          if (currentRoute) {
+            this.checkRoute(currentRoute, prevRoute);
+          }
+        }, this));
+      },
+
+      checkRoute: function(current, prev) {
+        if (current.$$route.requireLogin && !User.authenticated) {
+          $location.path('/');
+        }
+
+        // show/hide notifications
+        if (prev && prev.$$route && prev.$$route.requireLogin) {
+          $rootScope.notification = {
+            type: 'warning',
+            message: 'You must be authenticated to access this page, please login.'
+          };
+        } else {
+          delete $rootScope.notification;
+        }
       }
     };
 
--- client/source/js/services/check-authentication.spec.js
+++ client/source/js/services/check-authentication.spec.js
@@ -6,13 +6,22 @@ define([
 
   describe('CheckAuthentication Service', function () {
 
-    var rootScope, location, checkAuthentication;
+    var prevRoute, rootScope, location, checkAuthentication, User = {};
 
     beforeEach(module('app.services'));
 
     beforeEach(module(function ($provide) {
-      $provide.value('User', {});
+      $provide.value('User', User);
     }));
+
+    function goToRoute(currentRoute) {
+      rootScope.$broadcast('$routeChangeStart',
+        { '$$route': currentRoute },
+        { '$$route': prevRoute }
+      );
+
+      prevRoute = currentRoute;
+    }
 
     beforeEach(inject(function ($rootScope, $location, CheckAuthentication) {
       rootScope = $rootScope;
@@ -23,33 +32,39 @@ define([
     describe('start()', function () {
       beforeEach(inject(function ($httpBackend) {
         $httpBackend.when('GET', '/api/auth/check_authentication').respond('');
+        checkAuthentication.applicationIsBootstraped = true;
         checkAuthentication.start();
+        location.path('/randomPath');
       }));
 
-      it('redirect to "/" if user need authenticatin', function () {
-        expect(rootScope.notification).toBeUndefined();
-
-        rootScope.$broadcast('$routeChangeStart',
-          { '$$route': { requireLogin: true } }
-        );
-
+      it('redirect to "/" if user need authentication', function () {
+        expect(location.path()).not.toBe('/');
+        expect(User.authenticated).toBeUndefined();
+        goToRoute({ requireLogin: true });
         expect(location.path()).toBe('/');
+      });
 
-        rootScope.$broadcast('$routeChangeStart',
-          { '$$route': { requireLogin: false } },
-          { '$$route': { requireLogin: true} }
-        );
-
+      it('add notification if previous page was protected', function () {
+        expect(rootScope.notification).toBeUndefined();
+        goToRoute({ requireLogin: false }, { requireLogin: true });
         expect(rootScope.notification).toBeObject();
+      });
 
-        rootScope.$broadcast('$routeChangeStart',
-          { '$$route': { requireLogin: false } },
-          { '$$route': { requireLogin: false } }
-        );
+      it('remove notification next page load', function () {
+        rootScope.notification = { msg: 'foo' };
 
+        goToRoute({ requireLogin: false }, { requireLogin: false });
         expect(rootScope.notification).toBeUndefined();
       });
-    });
 
+      it('do nothing if user is authenticated', function () {
+        User.authenticated = true;
+        location.path('/account/dashboard');
+
+        expect(rootScope.notification).toBeUndefined();
+        goToRoute({ requireLogin: true }, { requireLogin: false });
+        expect(location.path()).toBe('/account/dashboard');
+      });
+    });
   });
 });
