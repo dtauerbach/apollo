c1fa509858dca8b0eed5e2366ac5ab38b30348a6
Jules Boussekeyt <jules.boussekeyt@gmail.com>
2014-01-05 21:35:26 +0100

    fixed karma:ci tests

--- client/karma.conf.js
+++ client/karma.conf.js
@@ -1,5 +1,5 @@
-// Karma configuration to test application source files
-// Usually used in development
+// Karma configuration to run tests against built application
+// Usually used in CI workflows
 module.exports = function (config) {
   config.set({
 
@@ -10,37 +10,23 @@ module.exports = function (config) {
 
 // list of files / patterns to load in the browser
     files           : [
-      'tests-main.js',
+      // all the sources, tests
+      'tests-main-compiled.js',
       { pattern: 'source/vendor/jasmine-matchers/dist/jasmine-matchers.js', included: true },
-      { pattern: 'source/js/**/*.js', included: false },
-      { pattern: 'source/js/**/*.map', included: false },
-      { pattern: 'source/vendor/**/*.js', included: false },
-      { pattern: 'source/vendor/**/*.map', included: false },
+      { pattern: 'source/js/config-require.js', included: false },
+      { pattern: 'source/js/**/*.spec.js', included: false },
+      { pattern: 'build/js/**/*', included: false },
+      { pattern: 'build/vendor/**/*.js', included: false },
+      { pattern: 'build/vendor/**/*.map', included: false }
     ],
 
 // list of files to exclude
-    exclude         : [
-      'source/js/main.js',
-      'source/vendor/**/*spec.js'
-    ],
+    exclude         : [],
 
-    preprocessors: {
-       // source files, that you wanna generate coverage for
-       // do not include tests or libraries
-       // (these files will be instrumented by Istanbul)
-       'source/js/modules/**/*.js': ['coverage']
-    },
-
-    // optionally, configure the reporter
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    },
-
-// test results reporter to use
+// use dots reporter, as travis terminal does not support escaping sequences
 // possible values: 'dots', 'progress', 'junit'
 // CLI --reporters progress
-    reporters       : ['dots', 'coverage'],
+    reporters       : ['dots'],
 
 // web server port
 // CLI --port 9876
@@ -72,15 +58,15 @@ module.exports = function (config) {
 // - PhantomJS
 // - IE (only Windows)
 // CLI --browsers Chrome,Firefox,Safari
-    browsers        : ['Chrome'],
+    browsers        : ['PhantomJS'],
 
 // If browser does not capture in given timeout [ms], kill it
 // CLI --capture-timeout 5000
-    captureTimeout  : 60000,
+    captureTimeout  : 10000,
 
 // Auto run tests on start (when browsers are captured) and exit
 // CLI --single-run --no-single-run
-    singleRun       : false,
+    singleRun       : true,
 
 // report which specs are slower than 500ms
 // CLI --report-slower-than 500
