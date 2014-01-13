e2f95996415949d2a82845628c60d6daddbabef8
Tiziano Perrucci <tiziano.perrucci@gmail.com>
2014-01-13 12:56:36 +0100

    Adds sample domain objects to setup script

--- server/src/setup.py
+++ server/src/setup.py
@@ -1,8 +1,9 @@
 from api import app
+
 print 'Setup database for app: %r' % app
 
 from db import db
-from repository import User
+from repository import User, Stream, Project
 import repository
 
 string = raw_input('Do you want to drop your existing database? [y/N] ').strip()
@@ -10,7 +11,44 @@ if string.lower() in ('y', 'yes'):
     db.drop_all()
 db.create_all()
 
-dan = User('dan@example.com', 'Dan', 'pass')
-dan.global_privacy = repository.PRIVACY_PUBLIC
-db.session.add(dan)
-db.session.commit()
+s = db.session
+
+s.add(User('dan@example.com', 'Dan', 'pass', repository.PRIVACY_PUBLIC))
+
+s.add(Stream('23andMe',
+             'http://23andme.com',
+             '23andMe_Logo_blog.jpg',
+             ','.join(['genes', 'genetics', 'biology', 'disease', '23andme']),
+             repository.CONNECTION_TYPE_SCRAPING))
+s.add(Stream('Jawbone UP',
+             'http://jawbone.com',
+             'jawbone_up.jpg',
+             ','.join(['jawbone', 'exercise', 'wellness', 'quantified self', 'jawbone up']),
+             repository.CONNECTION_TYPE_OAUTH))
+s.add(Stream('Electronic Health Record',
+             'http://en.wikipedia.org/wiki/Electronic_health_record',
+             'service_placeholder.png',
+             ','.join(['upload', 'manual', 'electronic health record']),
+             repository.CONNECTION_TYPE_MANUAL_UPLOAD))
+s.add(Stream('Kaiser Permenante',
+             'http://kaiser.com',
+             'service_placeholder.png',
+             ','.join(['insurance', 'care', 'kaiser permenante']),
+             repository.CONNECTION_TYPE_MANUAL_UPLOAD))
+s.add(Stream('Nike+',
+             'http://nike.com',
+             'nikeplus.jpg',
+             ','.join(['exercise', 'wellness', 'quantified self', 'running', 'nike']),
+             repository.CONNECTION_TYPE_OAUTH))
+
+s.add(Project('Sleep Study',
+              'http://some-study1.com',
+              'Study about people\'s sleeping behaviour'))
+s.add(Project('Fitness Study',
+              'http://some-study2.com',
+              'Study about people\'s health and strength conditions'))
+s.add(Project('Food Study',
+              'http://some-study3.com',
+              'Study about people\'s eating habits'))
+
+s.commit()
